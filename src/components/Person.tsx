a55d1e8affa6e07c6fa406993af899d058dc64bf
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-14 09:27:27 +0100

    country

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface Props {
   name: string;
@@ -7,9 +7,16 @@ interface Props {
 }
 
 export const Person: FC<Props> = ({ name, age, email }: Props) => {
+  const [country, setCountry] = useState<string | null>("");
   return (
     <div>
       <h1>{name}</h1>
+      <input
+        type="text"
+        placeholder="Your country"
+        onChange={(e) => setCountry(e.target.value)}
+      />
+      {country}
     </div>
   );
 };

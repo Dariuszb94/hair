861008d656a604cdec172b3c28031c125c2dd7a7
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-15 09:23:09 +0100

    context value

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React, { FC, createContext } from "react";
 import "./App.css";
 import { Person, HairColor } from "./components/Person";
-const AppContext = createContext(null);
+interface IAppContext {
+  name: string;
+  age: number;
+  country: string;
+}
+const AppContext = createContext<IAppContext | null>(null);
 const App: FC = () => {
+  const contextValue: IAppContext = {
+    name: "Darek",
+    age: 21,
+    country: "Poland",
+  };
   return (
-    <div className="App">
-      <Person
-        name="Darek"
-        age={27}
-        email="asdasd"
-        hairColor={HairColor.Blonde}
-      ></Person>
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <Person
+          name="Darek"
+          age={27}
+          email="asdasd"
+          hairColor={HairColor.Blonde}
+        ></Person>
+      </div>
+    </AppContext.Provider>
   );
 };
 
